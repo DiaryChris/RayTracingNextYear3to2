7d7ec68888dd3556d09235582958bae78282bf95
DiaryChris <383001400@qq.com>
2020-12-26 21:20:55 +0800

    Add base interact with box

---
+++ Assets/Scripts/Props/Box.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : Trigger
+{
+    public override void HandleTriggerEnterEvent()
+    {
+        base.HandleTriggerEnterEvent();
+
+        // Add new interactive function with box here
+    }
+
+
+    public override void HandleTriggerExitEvent()
+    {
+        base.HandleTriggerExitEvent();
+
+        // Add new interactive function with box here
+    }
+}
---
+++ Assets/Scripts/Props/Trigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Trigger : MonoBehaviour
+{
+
+    public Transform trans;
+
+    private Color prevColor;
+
+    private void Start()
+    {
+        if(trans == null)
+        {
+            trans = transform.parent;
+        }
+    }
+
+    public virtual void HandleTriggerEnterEvent()
+    {
+        prevColor = trans.GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
+        trans.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", Color.yellow);
+    }
+
+
+    public virtual void HandleTriggerExitEvent()
+    {
+        trans.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", prevColor);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+        Debug.Log(1);
+
+        HandleTriggerEnterEvent();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            return;
+        }
+        Debug.Log(2);
+        HandleTriggerExitEvent();
+    }
+}
