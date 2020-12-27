ba6a80a94fb919846af4c0f4ebbe295c0e9cff34
DiaryChris <383001400@qq.com>
2020-12-27 15:53:19 +0800

    Modify the highlight effect

--- Assets/Scripts/Interactive/Trigger.cs
+++ Assets/Scripts/Interactive/Trigger.cs
@@ -7,9 +7,14 @@ public class Trigger : MonoBehaviour
 {
 
     public Transform trans;
-    
+
+    public Color highlightColor = Color.yellow;
+    public float highlightOutlineWidth = 5.0f;
+    public Color highlightOutlineColor = new Color(1, 1, 1);
+
     private Color prevColor;
     private float prevOutlineWidth;
+    private Color prevOutlineColor;
 
     private void Start()
     {
@@ -21,11 +26,14 @@ public class Trigger : MonoBehaviour
 
     public virtual void HandleTriggerEnterEvent(Collider other)
     {
-        //prevColor = trans.GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
-        //trans.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", new Color32(225, 180, 64, 255));
+        prevColor = trans.GetComponent<MeshRenderer>().material.GetColor("_BaseColor");
+        trans.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", highlightColor);
 
         prevOutlineWidth = trans.GetComponent<MeshRenderer>().material.GetFloat("_OutlineWidth");
-        trans.GetComponent<MeshRenderer>().material.SetFloat("_OutlineWidth", 1.0f);
+        trans.GetComponent<MeshRenderer>().material.SetFloat("_OutlineWidth", highlightOutlineWidth);
+        prevOutlineColor = trans.GetComponent<MeshRenderer>().material.GetColor("_OutlineColorVertex");
+        trans.GetComponent<MeshRenderer>().material.SetColor("_OutlineColorVertex", highlightOutlineColor);
+
         trans.GetComponent<MeshRenderer>().material.SetInt("_UseRim", 1);
     }
     public virtual void HandleTriggerStayEvent(Collider other)
@@ -35,8 +43,9 @@ public class Trigger : MonoBehaviour
 
     public virtual void HandleTriggerExitEvent(Collider other)
     {
-        //trans.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", prevColor);
+        trans.GetComponent<MeshRenderer>().material.SetColor("_BaseColor", prevColor);
         trans.GetComponent<MeshRenderer>().material.SetFloat("_OutlineWidth", prevOutlineWidth);
+        trans.GetComponent<MeshRenderer>().material.SetColor("_OutlineColorVertex", prevOutlineColor);
         trans.GetComponent<MeshRenderer>().material.SetInt("_UseRim", 0);
 
     }
