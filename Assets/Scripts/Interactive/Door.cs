900ae62edd27caa55493b9c6dbedab7853136dc8
BossingChen <2365018489@qq.com>
2020-12-28 00:09:16 +0800

    - 门音效

--- Assets/Scripts/Interactive/Door.cs
+++ Assets/Scripts/Interactive/Door.cs
@@ -6,14 +6,19 @@ public class Door : Trigger
 {
     // Start is called before the first frame update
     public Animator vicAnim;
+    public AudioSource audio;
 
     public override void HandleTriggerStayEvent(Collider other)
     {
         base.HandleTriggerStayEvent(other);
 
         vicAnim.SetBool("isVictory", true);
+        audio.Play();
 
         GameManager.instance.Victory();
+
+
+
 
     }
 
