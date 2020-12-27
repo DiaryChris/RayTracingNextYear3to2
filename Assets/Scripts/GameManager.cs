ce1d365181d8be09871b8214b609329b83021356
DiaryChris <383001400@qq.com>
2020-12-27 23:20:12 +0800

    Add BGM Player & Link victory condition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,12 @@ using Common;
 public class GameManager : MonoSingleton<GameManager>
 {
 
+    public bool isVictory = false;
+
     /// <summary>
     /// （死亡时）重新加载
     /// </summary>
-    public static void RestartScene()
+    public void RestartScene()
     {
         //当前场景加载
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -21,9 +23,44 @@ public class GameManager : MonoSingleton<GameManager>
     /// <summary>
     /// 胜利时
     /// </summary>
-    public static void Victory()
+    public void Victory()
     {
         // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        isVictory = true;
         Debug.Log("Victory");
     }
+
+
+
+    //音乐播放
+    public AudioClip BGM;
+    public AudioSource BGMPlayer;
+
+
+    public void SetBGM(AudioClip clip)
+    {
+        BGM = clip;
+    }
+
+    //Audio Play
+    public void PlayBGM()
+    {
+        //Debug.Log("BGM Play");
+        BGMPlayer.clip = BGM;
+        BGMPlayer.Play();
+    }
+
+    public void PauseBGM()
+    {
+        BGMPlayer.Pause();
+    }
+    public void UnPauseBGM()
+    {
+        BGMPlayer.UnPause();
+    }
+    public void StopBGM()
+    {
+        BGMPlayer.Stop();
+    }
+
 }
